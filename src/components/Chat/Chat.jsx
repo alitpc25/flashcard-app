19fc0f8cfcb9856528c30ce9feaa40d960113e20
alitpc25 <alitpc2525@gmail.com>
2022-08-07 15:38:24 +0300

    Websocket connection port update

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -113,7 +113,7 @@ export default function Chat(props) {
 
     //Websocket.
     const connect = () => {
-        let socket = new SockJS('https://localhost:4000/ws');
+        let socket = new SockJS('https://vocabuilder.herokuapp.com/ws');
         stompClient = over(socket);
         stompClient.connect({}, function () {
             stompClient.subscribe('/topic/notifications', function (notificationResponse) {
