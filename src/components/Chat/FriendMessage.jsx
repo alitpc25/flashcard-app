01dfa5ef16fd0663be582debafe51a562c70c5f1
alitpc25 <alitpc2525@gmail.com>
2022-08-10 20:27:15 +0300

    some css changes to messaging component

--- src/components/Chat/FriendMessage.jsx
+++ src/components/Chat/FriendMessage.jsx
@@ -5,7 +5,7 @@ export default function ChatMessage(props) {
     const { message, userChatData, user } = props;
     return (
         <div className='friendMessage d-flex flex-column'>
-            <p style={{fontSize: "1rem"}} className='chatTime p-2'>{moment(message.sentAt).local().format('HH:mm:ss DD-MM-YYYY')}</p>
+            <p style={{fontSize:"0.75rem"}} className='chatTime p-2'>{moment(message.sentAt).local().format('HH:mm:ss DD-MM-YYYY')}</p>
             <p className='chatMessage p-2 h6'> {userChatData.friend.username === user.username ? userChatData.user.username : userChatData.friend.username} : {message.text} </p>
         </div>
     )
